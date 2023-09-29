5c2003f57f0a30002cd6df4f06ce75985c91c39c
Miguel Omar Zúñiga Rivera <miguel_omar30@hotmail.com>
2023-09-29 13:58:00 -0600

    color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,50 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import Link from "next/link";
 import { ArrowRight } from "lucide-react";
-import { buttonVariants } from "@/components/ui/button"
+import { buttonVariants } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <MaxWidthWrapper className="mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center">
-      <div className="mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50">
-        <p className="text-sm font-semibold text-gray-700">
-          Quill is now public!
+    <>
+      <MaxWidthWrapper className="mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center">
+        <div className="mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50">
+          <p className="text-sm font-semibold text-gray-700">
+            Quill is now public!
+          </p>
+        </div>
+        <h1 className="max-w-4xl text-5xl font-bold md:text-6xl lg:text-7xl">
+          Chat with your <span className="text-blue-600">documents</span> in
+          seconds.
+        </h1>
+        <p className="mt-5 max-w-prose text-zinc-700 sm:text-lg">
+          Quill allows you to chat with your PDF documents in real-time. It's a
+          simpler form of studying your documents. Simply upload yout file and
+          start asking questions right away.
         </p>
-      </div>
-      <h1 className="max-w-4xl text-5xl font-bold md:text-6xl lg:text-7xl">
-        Chat with your <span className="text-blue-600">documents</span> in seconds.
-      </h1>
-      <p className="mt-5 max-w-prose text-zinc-700 sm:text-lg">
-        Quill allows you to chat with your PDF documents in real-time. It's a simpler form of studying your documents. Simply upload yout file and start asking questions right away.
-      </p>
 
-      <Link className={buttonVariants({
-          size: "lg",
-          className: "mt-5"
-        })} href='/dashboard' target="_blank">
-        Get Started <ArrowRight className="ml-2 h-5 w-5"/>
-      </Link>
-    </MaxWidthWrapper>
-  )
+        <Link
+          className={buttonVariants({
+            size: "lg",
+            className: "mt-5",
+          })}
+          href="/dashboard"
+          target="_blank"
+        >
+          Get Started <ArrowRight className="ml-2 h-5 w-5" />
+        </Link>
+      </MaxWidthWrapper>
+
+      {/* Value proposition section*/}
+      <div>
+        <div className="relative isolate">
+          <div
+            aria-hidden="true"
+            className="pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+          >
+            <div className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradiebt-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50$-30rem)] sm:w-[72.1875rem]"></div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
